816dba607dd6f4ef9eec07e0e004989762ac3f64
Kc Leung <chiu-leung@Hotmail.com>
2018-05-24 18:14:14 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './app/nav/nav.component';
+import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { PositionsComponent } from './positions/positions.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    EmployeesComponent,
+    PositionsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
